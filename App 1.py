6dbe01d0b2ce08273bcdce7fb0ea6e53522b0861
jandroM-INS <alejandromontes.alum@insestatut.cat>
2026-01-19 00:47:56 +0100

    DEMO FUNCION LISTA USUARIOS

--- App 1.py
+++ App 1.py
@@ -2,22 +2,15 @@ documento = "clients.txt"
 clientsdoc=   open(documento,"a")
 
 #By Victor A.
-def ft_extract(documento):
-    extract1 = documento.split("\n")
+#Esta funcion desarrollada por Victor A. crea una LISTA con los nombres y DNI de cada individuo por separado
+def ft_extract():
+    clientsdoc = open("clients.txt", "r")
+    clientsdoc = clientsdoc.read()
+    extract1 = clientsdoc.split("\n")
     extract2 = []
     for i in range(len(extract1)):
         extract2.append(extract1[i].split(";"))
-    print(extract2)
     return (extract2)
-
-
-
-
-
-
-
-
-
 
 """
 def clientes_diccionario():
@@ -31,21 +24,35 @@ def clientes_diccionario():
     except FileNotFoundError:
         print("No existe el fichero")
 """
-def alta_clients():
+def ft_alta_clients():
         dni = int(input("Introduce el nif sin letra: "))
         dni = str(dni)
         nom = input("Introduce el nombre: ")
         clientsdoc = open("clients.txt","a")
         clientsdoc.write(dni+";" + nom+"\n")
         clientsdoc.close()
-def borrar_clientes(clientsdoc):
+
+def ft_listar_clients():
+    lista = ft_extract()
+    try:
+        for x in lista:
+            #de la lista, pilla el primer caracter y le da valor a la variable
+            dni = x[0]
+            nombre = x[1]
+            print("DNI:",dni,"NOMBRE:",nombre)
+        return
+    except FileNotFoundError:
+        print("Existe un error en el fichero")
+
+
+"""def ft_borrar_clientes(clientsdoc):
     clients = open("clients.txt", "a")
     clients4545 = {}
     for clientsdoc in range (clients):
         print(clients4545)
+"""
 
-
-def menu():
+def ft_menu():
     while True:
         try:
             texto=("""
@@ -59,16 +66,17 @@ ____________________________""")
             if menu == 0:
                 return (menu)
             elif menu == 1:
-                ft_extract
+                ft_listar_clients()
             elif menu == 2:
-                alta_clients()
+                ft_alta_clients()
             elif menu == 3:
-                borrar_clientes()
+                #ft_borrar_clientes()
+                print("hola")
             else:
                 print("Numero invalido")
         except:
             print("Caracter invalido")
 
 
-ala=menu()
+ala=ft_menu()
 print(ala)
