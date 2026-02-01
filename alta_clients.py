e767aaa49b2a8f2af7ba837d8ad178e4052532b4
Linded_ <91735533+Feimily@users.noreply.github.com>
2026-02-01 21:19:49 +0100

    merge app 1

--- alta_clients.py
+++ alta_clients.py
@@ -1,13 +1,5 @@
-def ft_extract():
-    # By Victor A.
-    # Esta funcion desarrollada por Victor A. crea una LISTA con los nombres y DNI de cada individuo por separado
-    clientsdoc = open("clients.txt", "r")
-    clientsdoc = clientsdoc.read()
-    extract1 = clientsdoc.split("\n")
-    extract2 = []
-    for i in range(len(extract1)):
-        extract2.append(extract1[i].split(";"))
-    return extract2
+import libft
+
 def ft_alta_clients(clientes_lista):
         dni = input("Introduce el NIF/DNI: ")
         #Verifica si el usuario con el DNI X esta en el documento
@@ -22,6 +14,7 @@ def ft_alta_clients(clientes_lista):
         clientsdoc.write(dni+";" + nom+"\n")
         clientsdoc.close()
         print("El usuario",nom,"a sido agregado")
+
 def ft_listar_clients(clientes_lista):
     lista = clientes_lista
     try:
@@ -34,6 +27,7 @@ def ft_listar_clients(clientes_lista):
         return
     except FileNotFoundError:
         print("Existe un error en el fichero")
+
 def ft_borrar_clientes(clientes_lista):
     dni_borrar = input("Cual es el DNI del usuario a borrar: ")
     nueva_lista = []
@@ -55,6 +49,7 @@ def ft_borrar_clientes(clientes_lista):
             documento.write(cliente[0] + ";" + cliente[1] + "\n")
     documento.close()
     return
+
 def ft_menu():
     while True:
             texto=("""
@@ -66,7 +61,8 @@ ____________________________""")
             print(texto)
             try:
                 menu=int(input("--> "))
-                clientes_lista=ft_extract()
+                # Llamada a la función desde el archivo libft
+                clientes_lista=libft.ft_extract()
                 if menu == 0:
                     menu="""¡ADIOS!"""
                     return menu
